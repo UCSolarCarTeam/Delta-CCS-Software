8e6d6500bb0033589402407655f865e19d65a7ef
Jordan Heinrichs <mail@jordanheinrichs.com>
2015-04-11 11:39:30 -0600

    Removed the graphic display code from the repo.
    Revert this commit if you ever want it back by:
    git revert <this commits hash>

--- Dashboard/Dashboard.h
+++ Dashboard/Dashboard.h
@@ -1,19 +1,15 @@
 /*-------------------------------------------------------
   Made for the dashboard OLED displays
   By Jordan Heinrichs on 2014-05-10 for the Solar Car Team
-  Copyright (c) 2014 by University of Calgary Solar Car Team 
+  Copyright (c) 2014 by University of Calgary Solar Car Team
 -------------------------------------------------------*/
 
 #ifndef DASHBOARD_H
 #define DASHBOARD_H
 
-//Mbed includes
 #include <mbed.h>
 
-//Solar Car includes
-#include <GraphicImageData.h>
 #include <CharDisplay.h>
-#include <GraphicDisplay.h>
 
 class VehicleData;
 
@@ -24,20 +20,16 @@ public:
             PinName chipSelectGraphicDisplay, PinName graphicDisplayDataSignal,
             PinName graphicDisplayReset,
             VehicleData& vehicleData);
-  
+
   void initAll();
   void displayTest();
-  void displayDashboard(); 
+  void displayDashboard();
 
 private:
   SPI serialCommunication_;
-  GraphicDisplay graphic_;
   CharDisplay charDisplay_;
-  GraphicImageData graphicImageData_;
 
   VehicleData& vehicleData_;
-
-  const unsigned char* findDigitNumber(int number);
 };
 
-#endif 
+#endif
