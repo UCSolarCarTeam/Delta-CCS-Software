2b708b85118ac470aba5b78b2baec2cd8086e28c
Enoch Tsang <etsang1@hotmail.com>
2015-09-21 23:13:07 -0600

    removed deadman's condition from drivercontrol

--- DriverControl/DriverControl.cpp
+++ DriverControl/DriverControl.cpp
@@ -67,8 +67,7 @@ void DriverControl::setSpeedAndCurrent()
    const VehicleData::CarDirection directionInput =
       static_cast<VehicleData::CarDirection>(directionInput_.read());
 
-   if (vehicleData_.deadmanPressed &&
-      isNewVehicleDirectionInputSafe(directionInput))
+   if (isNewVehicleDirectionInputSafe(directionInput))
    {
       if (isRegenBraking())
       {
